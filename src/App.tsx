9640488865a830ff0804191f99c52820d8b34d7f
lucas-dev3 <lucassdeveloper@gmail.com>
2025-01-06 13:28:41 -0300

    feat: copy cordinates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { PdfViewer } from './components/PdfViewer'
 import './App.css'
 import pdf from './assets/cte.pdf'
+import { notification } from 'antd'
 
 function App() {
+  const handleClick = (e: any) => {
+    const text = `X:${e.clientX}, Y:${e.clientY}`
+    navigator.clipboard.writeText(text)
+    notification.success({
+      message: 'Coordenadas copiadas',
+      description: text,
+      placement: 'topRight',
+    })
+  }
+
   return (
-    <div className="App">
+    <div className="App" onClick={(e)=> handleClick(e)}>
       <h1>PDF Navigation</h1>
       <PdfViewer pdfUrl={pdf} />
     </div>
